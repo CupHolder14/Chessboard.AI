f6a864e55f198e9ffb30feb167c18ab0ccff7df9
Joseph Youssouf <Josephyoussouf@hotmail.com>
2021-07-21 14:55:29 -0400

    Python Script Refactoring

--- AI_Testing/ChessEvents.py
+++ AI_Testing/ChessEvents.py
@@ -1,11 +1,18 @@
-events = []
+from serial.serialutil import XOFF
 
+
+events = []
+#We need to keep track of the most recent event and ensure that two initialtile events don't occur in sequence.
+values = []
 '''
 This function will return either the latest event from this list of events, or end the game immediately if GAMEOVER is on the list. 
 Or return a flag that tells us if it's an empty list.
 '''
-def get(events):
-    if events:
-        event = events[0]
-        events = events.pop(0)
-    return event
+LastEvent = "none"
+
+def get(events, values):
+    event = events[0]
+    events = events.pop(0)
+    value = values[0]
+    values = values.pop(0)
+    return event, value
